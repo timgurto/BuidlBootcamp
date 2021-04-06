2c1e9fb983d74c49e9788127a874a042234c479b
Tim Gurto <timgurto@gmail.com>
2021-04-07 08:26:48 +1000

    Remove repeated public-user data

--- DigitalCash/DigitalCash/UserWithSigningAuthority.cpp
+++ DigitalCash/DigitalCash/UserWithSigningAuthority.cpp
@@ -3,6 +3,9 @@
 
 #include "Transaction.h"
 
+UserWithSigningAuthority::UserWithSigningAuthority(const std::string& name)
+    : WeakUser({name}) {}
+
 void UserWithSigningAuthority::sign(class Transaction& transaction) const {
   transaction.m_signature = "Signed, " + name;
 }
--- DigitalCash/DigitalCash/UserWithSigningAuthority.h
+++ DigitalCash/DigitalCash/UserWithSigningAuthority.h
@@ -2,11 +2,12 @@
 
 #include <string>
 
+#include "WeakUser.h"
+
 // Represents an address that can hold coin, and the authority to sign
 // transactions sent from that address.
-class UserWithSigningAuthority {
+class UserWithSigningAuthority : public WeakUser {
  public:
-  std::string name;
-
+  UserWithSigningAuthority(const std::string& name);
   void sign(class Transaction& transaction) const;
 };

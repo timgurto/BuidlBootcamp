6855a41fa16e6826cdeab74bebda7dbc37c3cc4d
Tim Gurto <timgurto@gmail.com>
2021-04-07 13:49:00 +1000

    Separate the two User classes once again

--- DigitalCash/DigitalCash/UserWithSigningAuthority.cpp
+++ DigitalCash/DigitalCash/UserWithSigningAuthority.cpp
@@ -4,7 +4,7 @@
 #include "Transaction.h"
 
 UserWithSigningAuthority::UserWithSigningAuthority(const std::string& name)
-    : WeakUser({name}) {}
+    : name(name) {}
 
 void UserWithSigningAuthority::sign(class Transaction& transaction) const {
   transaction.m_signature =
--- DigitalCash/DigitalCash/UserWithSigningAuthority.h
+++ DigitalCash/DigitalCash/UserWithSigningAuthority.h
@@ -2,12 +2,12 @@
 
 #include <string>
 
-#include "WeakUser.h"
-
 // Represents an address that can hold coin, and the authority to sign
 // transactions sent from that address.
-class UserWithSigningAuthority : public WeakUser {
+class UserWithSigningAuthority {
  public:
   UserWithSigningAuthority(const std::string& name);
   void sign(class Transaction& transaction) const;
+
+  std::string name;
 };

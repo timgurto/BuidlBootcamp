2132abe742eb01595d6019691569830f5223ebac
Tim Gurto <timgurto@gmail.com>
2021-04-13 08:57:13 +1000

    Use stringstream to convert last two hex digits

--- DigitalCash/DigitalCash/Hexer.cpp
+++ DigitalCash/DigitalCash/Hexer.cpp
@@ -1,5 +1,7 @@
 #include "Hexer.h"
 
+#include <sstream>
+
 using namespace std::string_literals;
 
 std::string Hexer::convertHexToBinary(const std::string& hex) {
@@ -7,9 +9,12 @@ std::string Hexer::convertHexToBinary(const std::string& hex) {
 
   if (hex.size() > 2) return "\0\0"s;
 
-  auto binaryChar = char{};
-  binaryChar = (hex[0] - '0') * 16;
-  binaryChar += hex[1] - '0';
+  auto iss = std::istringstream{hex};
+
+  const auto digit1 = iss.get() - '0';
+  const auto digit2 = iss.get() - '0';
+
+  auto binaryChar = digit1 * 16 + digit2;
 
   auto ret = std::string{};
   ret.push_back(binaryChar);
